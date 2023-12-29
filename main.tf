29221b6e10ea525624f9b28c7ce2bc2d4c461fee
faith <faithkunjumon3@gmail.com>
2023-12-29 06:15:22 +0000

    first commit

--- main.tf
+++ main.tf
@@ -23,7 +23,8 @@ resource "aws_security_group" "frontend" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
+    
+    ingress {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
